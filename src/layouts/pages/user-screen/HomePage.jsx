a40ccab620fa3c3920a31448c21970a5efa8d7ad
Nguyen Tien Dat <dat86868389@gmail.com>
2023-12-10 22:56:27 +0700

    feat(HNLC-04): phát triển user header

--- src/layouts/pages/user-screen/HomePage.jsx
+++ src/layouts/pages/user-screen/HomePage.jsx
@@ -1,9 +1,9 @@
-const HomePage = () => {
+
+
+export default function HomePage() {
   return (
     <div className="w-full h-full bg-red-400 flex justify-between">
       <p className="bg-orange-400">trang chủ </p>
     </div>
   );
 };
-
-export default HomePage;
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,11 +1,14 @@
 import Image from "next/image";
+import LayoutUser from "@/layouts/components/LayoutUser";
 import { Inter } from "next/font/google";
 import HomePage from "@/layouts/pages/user-screen/HomePage";
 
 export default function Home() {
   return (
-    <main className="bg-slate-400">
       <HomePage />
-    </main>
   );
 }
+
+Home.getLayout = function getLayout(page) {
+  return <LayoutUser>{page}</LayoutUser>;
+};
